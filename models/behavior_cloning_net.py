ef083327ff3776ff985dd1cdae81e787d4684af5
Michael <28988176+mich-431@users.noreply.github.com>
2023-03-21 15:10:23 -0400

    reorganize repo

---
+++ models/behavior_cloning_net.py
@@ -0,0 +1,77 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import Dataset, DataLoader, random_split
+from tqdm import tqdm
+
+
+class BehaviorCloningModel(nn.Module):
+    """
+    """
+
+    def __init__(self, state_dim, action_dim, lr=1e-3):
+        super().__init__()
+        self.state_dim = state_dim
+        self.action_dim = action_dim
+        #self.loss_fcn = nn.MSELoss()
+
+        self.model =  nn.Sequential(
+          nn.Linear(self.state_dim, 256),
+          nn.ReLU(),
+          nn.Linear(256, 256),
+          nn.ReLU(),
+          nn.Linear(256, 256),
+          nn.ReLU(),
+          nn.Linear(256, self.action_dim)
+        )
+
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=lr)
+
+    def forward(self, state):
+        """
+        """
+        next_action = self.model(state)
+        return next_action
+    
+    def train_step(self, train_loader) -> float:
+        """
+        Performs an epoch train step.
+        :param model: Pytorch nn.Module
+        :param train_loader: Pytorch DataLoader
+        :param optimizer: Pytorch optimizer
+        :return: train_loss <float> representing the average loss among the different mini-batches.
+            Loss needs to be MSE loss.
+        """
+        train_loss = 0. 
+
+        for batch_idx, data in enumerate(train_loader):
+            self.optimizer.zero_grad()
+
+            # TODO: extract data correctly
+            state = data['state']
+            action = data['true_action']
+
+            pred_action = self.model(state)
+            loss = F.mse_loss(pred_action, action) 
+            loss.backward()
+            self.optimizer.step()
+
+            train_loss += loss.item()
+        return train_loss/len(train_loader)
+    
+    def train_model(self, train_dataloader, num_epochs=100):
+        """
+        Trains the given model for `num_epochs` epochs. Use SGD as an optimizer.
+        You may need to use `train_step` and `val_step`.
+        :param train_dataloader: Pytorch DataLoader with the training data.
+        :param num_epochs: <int> number of epochs to train the model.
+        :param lr: <float> learning rate for the weight update.
+        :return:
+        """
+        pbar = tqdm(range(num_epochs))
+        train_losses = []
+        for epoch_i in pbar:
+            train_loss_i = self.train_step(train_dataloader)
+            pbar.set_description(f'Train Loss: {train_loss_i:.4f}')
+            train_losses.append(train_loss_i)
+        return train_losses
